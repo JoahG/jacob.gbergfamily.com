3b437eb59d6109283aead3134a2c805c5eaeadd0
Joah Gerstenberg <joahd@gbergfamily.com>
2013-10-03 16:50:38 -0500

    More work... mostly stylistic

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 JacobBlog::Application.routes.draw do
+  get 'tags/:tag', to: 'posts#index', as: :tag
+  get 'type/:type', to: 'posts#index', as: :type
   root :to => 'posts#index'
+  resources :posts
 end
