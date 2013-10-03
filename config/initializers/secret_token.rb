b9e2da14dc19851f739f1bf0e5f6343854b4d1f6
Joah Gerstenberg <joahd@gbergfamily.com>
2013-10-02 21:06:58 -0500

    Init Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JacobBlog::Application.config.secret_token = '7046d1ac809b298c9b64a8abcd2cb8235fab0c0120dc37599e267c35bc5eb5bdfd1fcde5d564b897fd98e8d80b5bb6a695d8dc74ce8cf45d0f048ec7348b1bc4'
